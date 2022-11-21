6a4d067a4b0d8226130341998436985635ddba68
Bobby Carelse <bobbycarelse@gmail.com>
2022-11-21 14:15:29 +0200

    first commit

---
+++ src/components/footer/index.jsx
@@ -0,0 +1,5 @@
+export const Footer = () => {
+    return <div>
+        <p>Contact Us</p>
+    </div>
+}
---
+++ src/components/header/index.jsx
@@ -0,0 +1,7 @@
+import './header.css'
+
+export const Header = () => {
+    return <div className='header'>
+        <h1 className='header-text'>Example Application</h1>
+    </div>
+}
